5b6ca63ff1fdc546c6d52d4b4d2c6eb6a35d7b06
haemhariharan26 <113370966+haemhariharan26@users.noreply.github.com>
2023-12-09 18:45:54 -0500

    Files created

---
+++ contact_form_handler.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $subject = test_input($_POST["subject"]);
+    $message = test_input($_POST["message"]);
+
+    // Validate form data
+    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+        echo "All fields are required.";
+        exit();
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format.";
+        exit();
+    }
+
+   
+    $to = "heam@gmail.com"; 
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Subject: $subject\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Failed to send the message. Please try again later.";
+    }
+}
+
+// Function to sanitize form input
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+function openIndex() {
+    	 
+    var url = 'index.html';
+   
+        setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+}
+
+function openContact() {
+    	 
+  var url = 'contact-us.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+}
+function openGrapics()
+{
+var url = 'gallery.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);	
+}
+function openVideos()
+{
+	var url = 'videos.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+	
+}
+function openMyWork1()
+{
+	var url = 'mywork1.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+	
+}
+function openMyWork2()
+{
+	var url = 'mywork2.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+	
+}
+function openMyWork3()
+{
+	var url = 'videos.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+	
+}
+function openMyWork4()
+{
+	var url = 'mywork4.html';
+   
+        document.body.classList.add('fadeOut');
+
+         setTimeout(function() {
+            window.location.href = url;
+        }, 200);
+	
+}
+
+
